db49310e3db0fb3eb082dfde5106efa1414836f2
pramod.rao <pramod.rao@olacabs.com>
2020-02-17 09:37:38 +0530

    Weighted Quick Union

--- src/main/java/com/pramodrao/algorithms/QuickUnion.java
+++ src/main/java/com/pramodrao/algorithms/QuickUnion.java
@@ -1,5 +1,9 @@
 package com.pramodrao.algorithms;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+
 import java.util.Scanner;
 
 /**
@@ -8,16 +12,29 @@ import java.util.Scanner;
 public class QuickUnion {
 
     private int[] entries;
+    private int[] rootDistance;
 
     QuickUnion(int n) {
         entries = new int[n];
+        rootDistance = new int[n];
         for ( int i = 0; i < n; i++ ) {
             entries[i] = i;
+            rootDistance[i] = 1;
         }
     }
 
     private void union(int p, int q) {
         entries[p] = q;
+    }
+
+    private void weightedUnion(int p, int q) {
+        if ( rootDistance[p] < rootDistance[q] ){
+            entries[p] = q;
+            rootDistance[q] += rootDistance[p];
+        }
+        else
+            entries[q] = p;
+        rootDistance[p] += rootDistance[q];
     }
 
     private boolean connected(int p, int q) {
@@ -33,7 +50,29 @@ public class QuickUnion {
         return root;
     }
 
+    private void plotGraph() {
+
+        Graph graph = new SingleGraph("Quick Union");
+        graph.setStrict(false);
+        graph.setAutoCreate(true);
+
+        int numEntries = entries.length;
+        for ( int i = 0; i < numEntries; i++ ) {
+            String id = entries[i] +":" +i;
+            graph.addEdge(id, String.valueOf(entries[i]), String.valueOf(i));
+        }
+
+        for (Node node : graph) {
+            node.addAttribute("ui.label", node.getId());
+        }
+
+        graph.display(true);
+
+    }
+
     public static void main(String[] args) {
+
+        boolean runWeighted = true;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter number of elements.");
@@ -41,18 +80,33 @@ public class QuickUnion {
         QuickUnion quickUnion = new QuickUnion(numElements);
 
         System.out.println("Please enter the connections, separated by commas. Enter 'done' when finished.");
-        while (scanner.hasNext("[0-9]{1,2},[0-9]*")) {
-            String connection = scanner.next();
-            String[] elements = connection.split(",");
-            int start = Integer.parseInt(elements[0]);
-            int end = Integer.parseInt(elements[1]);
+        if ( runWeighted ) {
+            while (scanner.hasNext("[0-9]{1,2},[0-9]*")) {
+                String connection = scanner.next();
+                String[] elements = connection.split(",");
+                int start = Integer.parseInt(elements[0]);
+                int end = Integer.parseInt(elements[1]);
 
-            if ( quickUnion.connected(start, end)) System.out.println("Already connected.");
-            else {
-                quickUnion.union(start, end);
+                if ( quickUnion.connected(start, end)) System.out.println("Already connected.");
+                else {
+                    quickUnion.weightedUnion(start, end);
+                }
+            }
+        } else {
+            while (scanner.hasNext("[0-9]{1,2},[0-9]*")) {
+                String connection = scanner.next();
+                String[] elements = connection.split(",");
+                int start = Integer.parseInt(elements[0]);
+                int end = Integer.parseInt(elements[1]);
+
+                if ( quickUnion.connected(start, end)) System.out.println("Already connected.");
+                else {
+                    quickUnion.union(start, end);
+                }
             }
         }
 
+        quickUnion.plotGraph();
         System.out.println("6 and 2 are connected: " +quickUnion.connected(6,2));
     }
 }
